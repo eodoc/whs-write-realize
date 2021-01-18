f7a9c515342d01a3a318848b53d9ce7efff2d23b
胡少伟 <shaowei_hu@kingdee.com>
2021-01-18 09:25:15 +0800

    feat: Promise

---
+++ HPromise.js
@@ -0,0 +1,148 @@
+
+const PENDING = 'pending'
+const FULFILLED = 'fulfilled'
+const REJECTED = 'rejected'
+
+function HPromise (func) {
+
+  this.status = PENDING
+  this.onFulFillCallbacks = []
+  this.onRejectCallBacks = []
+
+  const self = this
+
+  function resolve (value) {
+    setTimeout(function () {
+      if (self.status === PENDING) {
+        self.value = value
+        self.status = FULFILLED
+        self.onFulFillCallbacks.forEach(cb => {
+          cb(self.value)
+        })
+      }
+    }, 0)
+  }
+
+  function reject (reason) {
+    setTimeout(function () {
+      if (self.status === PENDING) {
+        self.reason = reason
+        self.status = REJECTED
+        self.onRejectCallBacks.forEach(cb => {
+          cb(self.reason)
+        })
+      }
+    }, 0)
+  }
+
+  try {
+    func(resolve, reject)
+  } catch (error) {
+    reject.call(this, error)
+  }
+}
+
+HPromise.prototype.then = function (onFulFilled, onRejected) {
+  const self = this
+  if (this.status === PENDING) {
+    var pendingPromise = new HPromise(function (resolve, reject) {
+      self.onRejectCallBacks.push(function () {
+        setTimeout(function () {
+          try {
+            const res = onRejected(self.reason)
+            reject(res)
+          } catch (error) {
+            reject(error)
+          }
+        }, 0)
+      })
+      self.onFulFillCallbacks.push(function () {
+        setTimeout(function () {
+          try {
+            const res = onFulFilled(self.value)
+            resolve(res)
+          } catch (error) {
+            resolve(error)
+          }
+        }, 0)
+      })
+    })
+    return pendingPromise
+  }
+
+  if (this.status === REJECTED) {
+    const rejectPromise = new HPromise(function (resolve, reject) {
+      setTimeout(function () {
+        try {
+          const res = onRejected(self.value)
+          resolve(res)
+        } catch (error) {
+          reject(error)
+        }
+      }, 0)
+    })
+    return rejectPromise 
+  }
+
+  if (this.status === FULFILLED) {
+    const fulfillPromise = new HPromise(function (resolve, reject) {
+      setTimeout(function () {
+        try {
+          const res = onFulFilled(self.value)
+          resolve(res)
+        } catch (error) {
+          reject(error)
+        }
+      }, 0)
+    })
+    return fulfillPromise 
+  }
+}
+
+// 同步
+const p1 = new HPromise(function (resolve, reject) {
+  resolve('p1')
+}).then(function (result) {
+  console.log(result)
+})
+
+// 异步
+const p2 = new HPromise(function (resolve, reject) {
+  setTimeout(() => {
+    resolve('p2')
+  }, 500)
+}).then(function (result) {
+  console.log(result)
+})
+
+// 多个
+const p3 = new HPromise(function (resolve, reject) {
+  resolve('p3')
+})
+p3.then(function (result) {
+  console.log(`${result}-1`)
+})
+p3.then(function (result) {
+  console.log(`${result}-2`)
+})
+
+// 失败
+const p4 = new HPromise(function (resolve, reject) {
+  reject('p4')
+}).then(function (result) {
+  console.log(result)
+}, function (error) {
+  console.log(error)
+})
+
+// 语法错误
+const p5 = new HPromise(function (resolve, reject) {
+  const a = 1
+  a = 2
+}).then(function (result) {
+  console.log('p5 success')
+}, function (error) {
+  console.log('p5 error')
+})
+
+
